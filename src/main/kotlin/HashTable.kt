5b825391dc4669c459d30e380d91108eb73e5679
paulfischer <24pfischer@xbhs.net>
2023-11-08 08:03:26 -0500

    Separated out getKVPairs() Documented + reformated some methods

--- src/main/kotlin/HashTable.kt
+++ src/main/kotlin/HashTable.kt
@@ -1,13 +1,11 @@
 //TODO: Add iterator
-//TODO: Add method that returns all key value pairs(and make toString rely on it)
-
 import java.util.LinkedList
 
 class HashTable<K : Any, V>(private val hashFunc: (input: Any) -> Int, items: Collection<Pair<K,V>> = emptyList(), maxInitialStorage: Int = 16) {
     private val MAX_PERCENT_FULL: Double = 0.5
     private val SIZE_INCREASE_MULTIPLIER = 4
 
-    private var arr : Array<LinkedList<Pair<K, V>>?>  //17 is a very tasty number
+    private var arr : Array<LinkedList<Pair<K, V>>?>
     private val keys = mutableSetOf<K>()
     init {
         arr = Array(getArraySize(maxInitialStorage)) {null}
@@ -16,20 +14,27 @@ class HashTable<K : Any, V>(private val hashFunc: (input: Any) -> Int, items: Co
         }
     }
 
-    private fun getArraySize(maxOccupancy: Int) : Int {
-        return (maxOccupancy/MAX_PERCENT_FULL).toInt() //this will work unless you have a terrible hash function but is it really a hashing function at that point
-    }
+    //Pre-condition: Your hashing function is not a simple mod hash
+    private fun getArraySize(maxOccupancy: Int) : Int = (maxOccupancy/MAX_PERCENT_FULL).toInt()
 
+
+    //Post-condition: Returns the value if it is found, null otherwise
     operator fun get(key: K) : V? {
         val list = arr[getIndex(key)] ?: return null //should this really return null or should we have an empty spot object
         val collisionIndex = list.indexOfFirst { it.first == key }
         return if (collisionIndex != -1) list[collisionIndex].second else null
     }
 
+    //Post-condition: Returns true if successfully added, false if the key is already there
     operator fun set(key: K, value: V) : Boolean {
+        //Find spot
         val arrIndex = getIndex(key)
+
+        //Add a linked list if there isn't one there already
         if (arr[arrIndex] == null)
             arr[arrIndex] = LinkedList<Pair<K, V>>()
+
+        //Make sure the key isn't already in the linked list
         else {
             for(i in arr[arrIndex]!!.indices){
                 if(arr[arrIndex]!![i].first == key) {
@@ -37,6 +42,8 @@ class HashTable<K : Any, V>(private val hashFunc: (input: Any) -> Int, items: Co
                 }
             }
         }
+
+        //Add item
         arr[arrIndex]!!.addLast(Pair(key, value))
         if (keys.size >= MAX_PERCENT_FULL * arr.size) increaseSize()
         keys.add(key)
@@ -47,10 +54,7 @@ class HashTable<K : Any, V>(private val hashFunc: (input: Any) -> Int, items: Co
 
     private fun increaseSize(){
         //Store all the keys and values of the old hashmap
-        val keyValues = mutableSetOf<Pair<K, V>>()
-        for(k in keys){
-            keyValues.add(Pair(k, get(k)!!))
-        }
+        val keyValues = getKVPairs()
 
         //Clear and re-init everything
         keys.clear()
@@ -67,17 +71,16 @@ class HashTable<K : Any, V>(private val hashFunc: (input: Any) -> Int, items: Co
         keys.remove(key)
     }
 
-    fun size() : Int {
-        return keys.size
-    }
-
-    override fun toString(): String {
+    fun size() : Int = keys.size
+    fun getKVPairs() : MutableSet<Pair<K, V>>{
         val keyValues = mutableSetOf<Pair<K, V>>()
         for(k in keys){
             keyValues.add(Pair(k, get(k)!!))
         }
-        return keyValues.toString()
+        return keyValues
     }
+
+    override fun toString(): String = getKVPairs().toString()
 }
 
 fun main(args: Array<String>) {
