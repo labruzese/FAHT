a442d46e03cab8f20c3313e12ec29238b0578569
paulfischer <24pfischer@xbhs.net>
2023-11-08 12:30:58 -0500

    Starting writing hash

--- src/main/kotlin/FAH1.kt
+++ src/main/kotlin/FAH1.kt
@@ -1,5 +1,20 @@
 import kotlin.math.pow
 
-fun FAH1(arr: BooleanArray, sizeInPow: Int){
-    val maxSize = 1 shl sizeInPow
+//Pre-condition: sizeInPow >= 4
+fun FAH1(data: Int, sizeInPow: Int){
+    val maxSize = 2.0.pow(sizeInPow).toInt()
+
+    var hash = 0
+    for(i in 0..data.toString(2).length/2){
+        val p =
+        val chunk = data shr i*2
+        hash = hash xor chunk
+
+    }
+    //start with 0000
+    //in chunks of 2
+        //xor with data in chunks of length
+        //rotate-right by the nth prime's sqrt decimal to the nth digit
+            //n is the chunk num
+
 }
